76f6e0e60f1c10661d9256ff1f750dd85c3a1ad5
Michael Gregory <michael.gregory8584@gmail.com>
2020-06-01 17:27:31 -0500

    post works. can see new smurf on array issues: Smurf component cant seem to access the name prop

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import smurfReducer from '../reducers';
 
 const Smurf = props => {
     console.log(props, 'from Smurf')
     
     return (
         <>
-            <h2>{props.smurf[0].name}</h2>
+            <h2>{props.smurf.name}</h2>
         </>
     )
 }
--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { postNewSmurf } from '../actions'
 
-const SmurfForm = () => {
-    return null
+const SmurfForm = props=> {
+    // form field state
+    const [newSmurfName, setNewSmurfName] = useState()
+    const [newSmurfAge, setNewSmurfAge] = useState()
+    const [newSmurfHeight, setNewSmurfHeight] = useState()
+    
+    // maybe write a handler for this in reducer and pass it in via props?
+    const handleNameChange = e => {
+        setNewSmurfName(e.target.value);
+    }
+    const handleAgeChange = e => {
+        setNewSmurfAge(e.target.value);
+    }
+    const handleHeightChange = e => {
+        setNewSmurfHeight(e.target.value);
+    }
+
+    return (
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            // dispatch an ADD function here
+            props.postNewSmurf(newSmurfName, newSmurfAge, newSmurfHeight)
+            setNewSmurfName("")
+            setNewSmurfAge("")
+            setNewSmurfHeight("")
+        }}>
+            <input
+                onChange={handleNameChange}
+                type="text"
+                name="newSmurfName"
+                value={newSmurfName}
+            />
+            <input
+                onChange={handleAgeChange}
+                type="text"
+                name="newSmurfAge"
+                value={newSmurfAge}
+            />
+            <input
+                onChange={handleHeightChange}
+                type="text"
+                name="newSmurfHeight"
+                value={newSmurfHeight}
+            />
+            <button>Add Smurf</button>
+           
+        </form>
+    )
+}
+const mapStateToProps = state => {
+    return {
+        ...state
+    }
 }
 
-export default SmurfForm
+
+export default connect(mapStateToProps, {postNewSmurf})(SmurfForm)
